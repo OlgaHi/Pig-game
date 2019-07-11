4b807f128d55299fbd319de752e0887fdaf57000
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2019-07-11 00:05:33 -0700

    fixed current, add active player

--- assets/app.js
+++ assets/app.js
@@ -30,7 +30,28 @@ var diceDOM = document.querySelector('.dice');
 diceDOM.style.display = 'block';
 diceDOM.src = 'dice-' + dice + '.png';
 
-//3. Update the round score if the rolled number wasNOT a 1
+//3. Update the round score if the rolled number was NOT a 1
+if (dice !== 1){
+  //Add score
+  roundScore += dice;
+  document.querySelector('#current-' + activePlayer).textContent = roundScore;
+}else{
+  //Next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active')
+
+  document.querySelector('.dice').style.display = 'none';
+
+  //document.querySelector('.player-0-panel').classList.remove('active');
+  //document.querySelector('.player-1-panel').classList.add('active')
+
+
+}
 
 
 });

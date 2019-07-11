aac94ae2bd325fc9140d6a57bfbb46ed6bc83fb4
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2019-07-11 16:27:58 -0700

    added  hold btn and new game btn

--- assets/app.js
+++ assets/app.js
@@ -8,20 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+var scores, roundScore, activePlayer, gamePlaying;
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+if(gamePlaying){
 //1. Random number
 var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -37,27 +29,67 @@ if (dice !== 1){
   document.querySelector('#current-' + activePlayer).textContent = roundScore;
 }else{
   //Next player
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
-  roundScore = 0;
-  document.getElementById('current-0').textContent = '0';
-  document.getElementById('current-1').textContent = '0';
-
-  document.querySelector('.player-0-panel').classList.toggle('active');
-  document.querySelector('.player-1-panel').classList.toggle('active')
-
-  document.querySelector('.dice').style.display = 'none';
-
-  //document.querySelector('.player-0-panel').classList.remove('active');
-  //document.querySelector('.player-1-panel').classList.add('active')
-
-
+  nextPlayer();
 }
-
-
+}
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+if(gamePlaying){
+// Add current score to global score
+scores[activePlayer] += roundScore;
+// Update the UI
+document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+// Check if player won the game
+if (scores[activePlayer] >=20){
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    gamePlaying = false;
+}else{
+//Next player
+nextPlayer();
+}
+}
+});
 
+function nextPlayer(){
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+  
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active')
+  
+    document.querySelector('.dice').style.display = 'none'; 
+}
 
+document.querySelector('.btn-new').addEventListener('click', init);
+   
+function init(){
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+};
 
 //Setter
 //document.querySelector('#current-'+ activePlayer).textContent = dice;
@@ -66,6 +98,8 @@ if (dice !== 1){
 //Getter(only for read)
 //var x = document.querySelector('#score-0').textContent;
 //console.log(x);
+//document.querySelector('.player-0-panel').classList.remove('active');
+//document.querySelector('.player-1-panel').classList.add('active')
 
 
 
